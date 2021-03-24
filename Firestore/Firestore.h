6dd898b2d02fc9849d09eb9aeb79b1ea3fe90dea
Julian Panucci <julian.panucci@underarmour.com>
2021-03-24 15:32:25 -0500

    Initial Commit

---
+++ Firestore/Firestore.h
@@ -0,0 +1,18 @@
+//
+//  Firestore.h
+//  Firestore
+//
+//  Created by Julian Panucci on 3/24/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Firestore.
+FOUNDATION_EXPORT double FirestoreVersionNumber;
+
+//! Project version string for Firestore.
+FOUNDATION_EXPORT const unsigned char FirestoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Firestore/PublicHeader.h>
+
+
