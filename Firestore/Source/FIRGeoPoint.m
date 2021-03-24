033bca0b2fc393a33a5afff62f93c6f1ca106c20
Julian Panucci <julian.panucci@underarmour.com>
2021-03-24 16:17:40 -0500

    Fixing gepoint

--- Firestore/Source/FIRGeoPoint.m
+++ Firestore/Source/FIRGeoPoint.m
@@ -11,19 +11,9 @@
 
 - (instancetype)initWithLatitude:(double)latitude longitude:(double)longitude {
   if (self = [super init]) {
-	if (latitude < -90 || latitude > 90 || !isfinite(latitude)) {
-	  ThrowInvalidArgument("GeoPoint requires a latitude value in the range of [-90, 90], "
-						   "but was %s",
-						   latitude);
-	}
-	if (longitude < -180 || longitude > 180 || !isfinite(longitude)) {
-	  ThrowInvalidArgument("GeoPoint requires a longitude value in the range of [-180, 180], "
-						   "but was %s",
-						   longitude);
-	}
-
-	_latitude = latitude;
-	_longitude = longitude;
+	  NSLog(@"words :: %@", latitude);
   }
   return self;
 }
+
+@end
